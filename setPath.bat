6b03cd44ba0479d4f01aa0db41b01d43b5d1e40d
Uthai Khantamongkhon (SDM) <uthai.khantamongkhon.a7p@ap.denso.com>
2024-03-28 18:26:16 +0700

    first commit

---
+++ setPath.bat
@@ -0,0 +1,3 @@
+@cd D:\nfc-scan-client
+@set PATH=D:\node-v20.9.0-win-x64;%PATH%
+@cmd.exe /K
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const App = () => {
+  const [scannedData, setScannedData] = useState('');
+
+  const handleScan = async () => {
+    try {
+      if ('NDEFReader' in window) {
+        const ndef = new NDEFReader();
+
+        ndef.addEventListener('reading', event => {
+          const scannedData = event.message.records[0].data;
+          setScannedData(scannedData);
+
+          axios.post('/api/nfc', {data: scannedData})
+          .then(res => {
+            console.log(res.data);
+          })
+          .catch(error => {
+            console.error(error);
+          })
+        });
+
+        // Start reading NFC
+        await ndef.scan();
+      } else {
+        console.log('NFC not supported on this device');
+      }
+
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <h1>NFC Scanner</h1>
+      <button onClick={handleScan}>Scan NFC</button>
+      {scannedData && <p>Scanned Data: {scannedData}</p>}
+    </>
+  )
+}
+
+export default App
